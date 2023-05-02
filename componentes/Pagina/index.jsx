043942aed710181923bcc0c1d23786fe59a4b5d9
Érica Garcez <112111829+Japolina@users.noreply.github.com>
2023-05-02 19:25:27 -0300

    Add files via upload

---
+++ componentes/Pagina/index.jsx
@@ -0,0 +1,18 @@
+import Aside from '../Aside'
+import Header from '../Header'
+import Main from '../Main'
+import Footer from '../Footer'
+
+function Pagina() {
+    return (
+        <>
+        <Header />
+        <Aside />
+        <Main />
+        <Footer />
+        </>
+        
+    )
+}
+
+export default Pagina
